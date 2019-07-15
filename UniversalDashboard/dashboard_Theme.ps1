dbd9452ad6c1a07ec4f7132d90883da8c3eb3d2a
Constantin Hager <constantin.hager@outlook.de>
2019-07-15 21:47:33 +0200

    Added a theme support to dashboard.ps1

---
+++ UniversalDashboard/dashboard_Theme.ps1
@@ -0,0 +1,27 @@
+$ConfigurationFile = Get-Content (Join-Path $PSScriptRoot dbconfig.json) | ConvertFrom-Json
+
+Import-Module (Join-Path $PSScriptRoot $ConfigurationFile.dashboard.rootmodule) -ErrorAction Stop -Force
+
+. (Join-Path -Path $PSScriptRoot -ChildPath "themes\*.ps1")
+
+$PageFolder = Get-ChildItem (Join-Path $PSScriptRoot pages)
+
+$Pages = Foreach ($Page in $PageFolder)
+{
+    . $Page.FullName
+}
+
+$Initialization = New-UDEndpointInitialization -Module @(Join-Path $PSScriptRoot $ConfigurationFile.dashboard.rootmodule)
+
+$DashboardParams = @{
+    Title                  = $ConfigurationFile.dashboard.title
+    Theme                  = $SampleTheme
+    Pages                  = $Pages
+    EndpointInitialization = $Initialization
+}
+
+Get-UDDashboard | Stop-UDDashboard
+
+$MyDashboard = New-UDDashboard @DashboardParams
+
+Start-UDDashboard -Port $ConfigurationFile.dashboard.port -Dashboard $MyDashboard -Name $ConfigurationFile.dashboard.title -Wait
